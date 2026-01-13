de3e3906a20385e057d75bd9ea42b7e49b7ae4bc
Roman <khasa11@mail.ru>
2026-01-14 00:13:37 +0300

    Добавил project descitption и улучшил aside

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,19 +1,23 @@
 import React from 'react';
 import './styles/App.css';
-import LoginView from './Views/LoginView';
 import { Routes, Route } from 'react-router-dom';
 import { ConfigProvider, theme } from 'antd';
+
+import LoginView from './Views/LoginView';
+import ProjectView from './Views/ProjectView';
 
 function App() {
   return (
     <ConfigProvider
       theme={{
         algorithm: theme.darkAlgorithm,
+        // algorithm: theme.defaultAlgorithm,
       }}
     >
       <div className="App">
         <Routes>
           <Route path="/login" element={<LoginView />} />
+          <Route path="/project" element={<ProjectView />} />
           <Route path="/" element={<div>Главная страница</div>} />
           <Route path="*" element={<div>404 — Страница не найдена</div>} />
         </Routes>
